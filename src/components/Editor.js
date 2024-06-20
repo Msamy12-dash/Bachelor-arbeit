7d8010ade4adcf8815eec575ded5a4a0a5c6093d
Samuel Richter <bt719826@uni-bayreuth.de>
2024-06-20 17:16:02 +0200

    Simplified Code & Mark text selection feature

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactQuill from "react-quill"
+import ReactQuill, {Quill} from "react-quill"
 import 'react-quill/dist/quill.snow.css'
 import "../styles.css"
 
@@ -9,41 +8,44 @@ class Editor extends Component{
     constructor(props){
       super(props);
       this.state = {
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elit"
+        selectedRange: ["null", "null"]
       }
+
+      this.quill = null;
       this.handleChange = this.handleChange.bind(this);
-      this.modules = {
-        toolbar: [
-          [{ 'header': [1, 2, false] }],
-          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-          ['link', 'image'],
-          ['clean']
-        ]
-      };
-   
-      this.formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-      ]
+
+      this.editorRef = React.createRef();
     }
-    
+
+    componentDidMount() {
+        this.quill = new Quill(this.editorRef.current, {
+        theme: 'snow'
+      });
+      }
+
+
+
+    handleOnClick = () => {
+      const selection = this.quill.getSelection(); // Get range the user selected
+      this.setState({selectedRange: [selection.index, selection.length]})
+
+      this.quill.formatText(selection.index, selection.length, {
+        'background': '#FFFF00' // Yellow mark
+      });
+    }
+
 
     handleChange(value){
       this.setState({text: value});
     }
-    
-  
-  
+
+
     render(){
       return <div>
-        <ReactQuill placeholder={this.state.text}
-          modules={this.modules}
-          formats={this.formats}
-          onChange={this.handleChange} />
-        <button onClick={this.getSelection_}>Get Selection</button>
+        <div className='quill' ref={this.editorRef} />
+        <h6>Range:</h6>
+        <p>index={this.state.selectedRange[0]}, length={this.state.selectedRange[1]}</p>
+        <button onClick={this.handleOnClick}>Mark selected range</button>
       </div>
     }
   }
