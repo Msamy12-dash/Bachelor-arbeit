41569988f0e0955424d8e3a81985129ed4d6cd7d
PaulaHummel <paula.hummel@uni-bayreuth.de>
2024-07-04 16:51:11 +0200

    saving, deleting, editing comments in database

---
+++ pages/api/save-comment.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
+
+interface Comment {
+  key: number;
+  name: string;
+  content: string;
+  date: string; 
+  upvotes: number;
+  isTextSpecific: boolean;
+  selectedText: string;
+  index: number;
+  length: number;
+  history: string[]; 
+  replies: Comment[];
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { room, comment }: { room: string; comment: Comment } = req.body;
+
+    const client = new MongoClient('mongodb+srv://inlp:INLP123@cluster0.vpm9s6o.mongodb.net/mainText?retryWrites=true&w=majority&appName=Cluster0');
+
+    try {
+      await client.connect();
+      //soll die datenbank mainText heißen??
+      const db = client.db('mainText');
+      const result = await db.collection('comments').insertOne(comment);
+      res.status(200).json({ message: 'Comment saved successfully', result });
+    } catch (error) {
+      console.error('Failed to save comment:', error);
+      res.status(500).json({ error: 'Failed to save comment' });
+    } finally {
+      await client.close();
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
