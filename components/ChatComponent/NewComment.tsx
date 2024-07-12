5388174e2ad68d598b08c4a17a66794b6003f746
PaulaHummel <paula.hummel@uni-bayreuth.de>
2024-07-12 11:18:03 +0200

    fixbug

--- components/ChatComponent/NewComment.tsx
+++ components/ChatComponent/NewComment.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
-import React, { Component, ChangeEvent } from "react";
+import React, { Component, ChangeEvent } from 'react';
 
 interface NewCommentProps {
-  addComment: (comment: Comment) => void;
-  cancel: () => void;
+    addComment: (comment: Comment) => void;
+    cancel: () => void;
 }
 
 interface NewCommentState {
-  content: string;
+    content: string;
 }
 
 interface Comment {
@@ -27,53 +26,21 @@ interface Comment {
 }
 
 class NewComment extends Component<NewCommentProps, NewCommentState> {
-  textareaRef: React.RefObject<HTMLTextAreaElement>;
+    textareaRef: React.RefObject<HTMLTextAreaElement>;
 
-  constructor(props: NewCommentProps) {
-    super(props);
-    this.state = {
-      content: "",
-    };
-    this.textareaRef = React.createRef();
-  }
-
-  handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({
-      content: event.target.value,
-    });
-  };
-
-  handleOnClick = () => {
-    const textarea = this.textareaRef.current;
-
-    if (textarea && textarea.value !== "") {
-      const { content } = this.state;
-      const name = "Name"; // Placeholder for the name
-      const date = new Date().toLocaleDateString();
-      const comment: Comment = {
-        key: 0,
-        name: name,
-        content: content,
-        date: date,
-        upvotes: 0,
-        isTextSpecific: false,
-        selectedText: "",
-        index: 0,
-        length: 0,
-        history: [],
-        replies: [],
-        parentKey: null,
-        canReply: true,
-      };
-
-      this.props.addComment(comment); // Nur den Hauptkommentar hinzufügen
-      textarea.value = "";
-      this.setState({ content: "" });
+    constructor(props: NewCommentProps) {
+        super(props);
+        this.state = {
+            content: ''
+        };
+        this.textareaRef = React.createRef();
     }
-  };
 
-  render() {
-    const { cancel } = this.props;
+    handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({
+            content: event.target.value
+        });
+    }
 
     handleOnClick = () => {
         const textarea = this.textareaRef.current;
@@ -110,5 +77,7 @@ class NewComment extends Component<NewCommentProps, NewCommentState> {
 }
 
 export default NewComment;
+
+
 
 
