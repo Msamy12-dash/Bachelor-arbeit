db1175d915e5a6d2486bc7065bb6370e074d5e9b
Daniel <hadirahardjo.daniel@gmail.com>
2024-07-20 12:19:48 +0200

    Reenabling previous functionality.

--- party/voteserver.ts
+++ party/voteserver.ts
@@ -1,57 +1,57 @@
-// /* eslint-disable prettier/prettier */
-// import type * as Party from "partykit/server";
+/* eslint-disable prettier/prettier */
+import type * as Party from "partykit/server";
 
-// import { Poll } from "./types";
+import { Poll } from "./types";
 
-// export default class VoteServer implements Party.Server {
+export default class VoteServer implements Party.Server {
   
-//   constructor(readonly party: Party.Party) {}
+  constructor(readonly party: Party.Party) {}
 
-//   poll: Poll | undefined;
+  poll: Poll | undefined;
 
-//   async onRequest(req: Party.Request) {
-//     if (req.method === "POST") {
-//       const poll = (await req.json()) as Poll;
+  async onRequest(req: Party.Request) {
+    if (req.method === "POST") {
+      const poll = (await req.json()) as Poll;
 
-//       console.log(poll);
+      console.log(poll);
       
-//       this.poll = { ...poll, votes: poll.options.map(() => 0) };
-//       this.savePoll();
-//     }
+      this.poll = { ...poll, votes: poll.options.map(() => 0) };
+      this.savePoll();
+    }
 
-//     if (this.poll) {
-//       return new Response(JSON.stringify(this.poll), {
-//         status: 200,
-//         headers: { "Content-Type": "application/json" },
-//       });
-//     }
+    if (this.poll) {
+      return new Response(JSON.stringify(this.poll), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
 
-//     return new Response("Not found", { status: 404 });
-//   }
+    return new Response("Not found", { status: 404 });
+  }
 
-//   async onMessage(message: string) {
-//     if (!this.poll) return;
+  async onMessage(message: string) {
+    if (!this.poll) return;
 
-//     const event = JSON.parse(message);
+    const event = JSON.parse(message);
 
-//     if (event.type === "vote") {
-//       this.poll.votes![event.option] += 1;
-//       this.party.broadcast(JSON.stringify(this.poll));
-//       this.savePoll();
-//       console.log("On message");
-//       console.log(this.poll);
+    if (event.type === "vote") {
+      this.poll.votes![event.option] += 1;
+      this.party.broadcast(JSON.stringify(this.poll));
+      this.savePoll();
+      console.log("On message");
+      console.log(this.poll);
 
-//     }
-//   }
+    }
+  }
 
-//   async savePoll() {
-//     if (this.poll) {
-//       await this.party.storage.put<Poll>("poll", this.poll);
-//     }
-//   }
+  async savePoll() {
+    if (this.poll) {
+      await this.party.storage.put<Poll>("poll", this.poll);
+    }
+  }
 
-//   async onStart() {
-//     this.poll = await this.party.storage.get<Poll>("poll");
-//   }
-// }
+  async onStart() {
+    this.poll = await this.party.storage.get<Poll>("poll");
+  }
+}
 
