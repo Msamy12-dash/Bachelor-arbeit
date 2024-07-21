bd7dc6187c06e6a60b8d7642b29e3184475fba4d
Daniel <hadirahardjo.daniel@gmail.com>
2024-07-21 10:00:52 +0200

    Edit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,31 @@ import YPartyKitProvider from "y-partykit/provider";
 import useYProvider from "y-partykit/react";
 import { PARTYKIT_HOST } from "./env";
 import usePartySocket from "partysocket/react";
+import { connect } from "http2";
 
 export default function IndexPage() {
   const [currentRoom, setCurrentRoom] = useState("default");
 
-  const yProvider = useYProvider({
+  const yProvider = useRef<YPartyKitProvider | null>(null);
+
+  useEffect(() => {
+    // Cleanup function to disconnect the previous provider
+    return () => {
+      if (yProvider.current) {
+        yProvider.current.disconnect();
+        yProvider.current = null;
+      }
+    };
+  }, [currentRoom]);
+  
+  const actualYProvider = useYProvider({
     host: PARTYKIT_HOST,
     party: "editorserver",
     room: currentRoom,
   });
+
+  // Assign the new provider to the ref
+  yProvider.current = actualYProvider;
 
   const renderCount = useRef(0);
   const mountCount = useRef(0);
@@ -32,7 +48,8 @@ export default function IndexPage() {
 
   return (
     <DefaultLayout currentRoom={currentRoom} setCurrentRoom={setCurrentRoom} >
-        <EditorPage currentRoom={currentRoom} yProvider={yProvider} />
+        {/* <EditorPage currentRoom={currentRoom} yProvider={yProvider} /> */}
+        <div>Index Page</div>
     </DefaultLayout>
   );
 }
